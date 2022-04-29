fe0968033d3579620a2520660e73feea1f36dc27
Aaron Mai <48746713+aarontmai@users.noreply.github.com>
2022-04-28 17:08:06 -0700

    Add files via upload

--- websitess.py
+++ websitess.py
@@ -13,7 +13,7 @@ def read_file(filename):
     with fs.open(filename) as f:
         return f.read().decode("utf-8")
 
-content = read_file("indexed-faces490/streamlitcsv/myfile.csv")
+content = read_file("indexed-faces490/myfile.csv")
 
 # Print results.
 for line in content.strip().split("\n"):
